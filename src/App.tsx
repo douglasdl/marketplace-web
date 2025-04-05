1b9fbb3cee478c21f409fb0147ac25852bd2dab0
Douglas Dias Leal <douglas_san@hotmail.com>
2025-04-05 17:44:42 +0900

    chore: add typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 export function App() {
   return (
-    <div className="flex items-center justify-center bg-background text-gray-500 h-screen">App</div>
+    <div className="flex flex-col items-center justify-center bg-background text-gray-500 h-screen">
+      <span className="text-title-lg font-bold">texto</span>
+      <span className="text-title-md font-bold">texto</span>
+      <span className="text-title-sm font-bold">texto</span>
+      <span className="text-subtitle font-semibold">texto</span>
+      <span className="text-body-md font-normal">texto</span>
+      <span className="text-body-sm font-normal">texto</span>
+      <span className="text-body-xs font-normal">texto</span>
+      <span className="text-label-md font-normal uppercase">texto</span>
+      <span className="text-label-sm font-medium uppercase">texto</span>
+      <span className="text-action-md font-medium">texto</span>
+      <span className="text-action-sm font-medium">texto</span>
+    </div>
   )
 }
